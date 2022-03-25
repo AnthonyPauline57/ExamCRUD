eb931e484986fe1b451e9b505d694eddc8763bed
AnthonyPauline57 <95024076+AnthonyPauline57@users.noreply.github.com>
2022-03-26 00:03:13 +0100

    exam

---
+++ data/liste.js
@@ -0,0 +1,42 @@
+const srcListe = [
+    {
+      id:0,
+      name: 'LostArk',
+      prix: 9.90+` €`,
+      dispo:true
+    }, {
+      id:1,
+      name: 'Dying Light 2',
+      prix: 54.90+` €`,
+      dispo:true
+   
+    }, {
+      id:2,
+      name: 'Son of The Forest',
+      prix: 38.75+` €`,
+      dispo:false
+     
+    },
+    {
+      id:3,
+      name: 'Rocket League',
+      prix: 19.90+` €`,
+      dispo:true
+    }, {
+      id:4,
+      name: 'Cyberpunk 2077',
+      prix: 2.77+` €`,
+      dispo:false
+   
+    }, {
+      id:5,
+      name: 'Overwatch',
+      prix: 29.50+` €`,
+      dispo:true
+     
+    }
+    
+  ]
+  
+  module.exports = srcListe;
+
